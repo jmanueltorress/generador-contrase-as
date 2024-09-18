fe51c45917730bd4518054baf7fa073241ef822e
jmanueltorress <tjmanuel354@gmail.com>
2024-09-17 18:04:15 -0600

    add button clean and char esp in pass

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let cantidad = document.getElementById("cantidad");
 // variables a utilizar en funcion boton generar
 let boton = document.getElementById("generar");
 let contrasena=document.getElementById("contrasena");
-const cadenaCaracteres ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789/*=)?¡(!#$%&";
+const cadenaCaracteres ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
+const caracteresEspeciales = "!@#$%^&*()_+[]{}|;:,.<>?"; 
   function generarPass() {
     /* El input maneja los caracteres ingresados como tipo de dato texto, 
       entonces hay que convertir el valor ingresado a número */
@@ -13,12 +14,20 @@ const cadenaCaracteres ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123
     // Validación de caracteres y creacion de contraseña
     if (numeroDigitado >= 7) {
       // Generar contraseña
-      const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
+      
       let password = '';
-      for (let i = 0; i < numeroDigitado; i++) {
+      for (let i = 0; i < numeroDigitado-1; i++) {
         let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        let charEspecial = caracteresEspeciales[Math.floor(Math.random() * caracteresEspeciales.length)];
         password += caracterAleatorio;
       }
+       // Insertar al menos un carácter especial en una posición aleatoria
+       let charEspecial = caracteresEspeciales[Math.floor(Math.random() * caracteresEspeciales.length)];
+       let posicionAleatoria = Math.floor(Math.random() * password.length);
+       
+       // Insertar el carácter especial en la posición aleatoria
+       password = password.slice(0, posicionAleatoria) + charEspecial + password.slice(posicionAleatoria);
+       
       console.log('Password generada: ' + password);
       contrasena.value = password; // Asignar la contraseña al input
     } else {
@@ -27,4 +36,11 @@ const cadenaCaracteres ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123
       contrasena.value = ''; // Limpiar el campo de la contraseña
     }
   }
+  
+
+  function cleanPass() {
+   
+      contrasena.value = ''; // Limpiar el campo de la contraseña
+    }
+  
   
---
+++ readme.md
@@ -0,0 +1,3 @@
+<!-- Proyecto: Instrucciones objetivo -->
+1. Indicar cantidad de caracteres a traves de un input -- Cantidad minima: 7
+    ingresar numero - campo caracter(input) - almacenarlo en variable
